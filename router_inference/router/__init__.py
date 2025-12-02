4150d24cf10467d6063a1239d54959aebcced101
yl231 <yifan21lu@gmail.com>
2025-12-02 17:57:18 -0600

    tiny fix

--- router_inference/router/__init__.py
+++ router_inference/router/__init__.py
@@ -5,6 +5,5 @@
 
 from router_inference.router.base_router import BaseRouter
 from router_inference.router.example_router import ExampleRouter
-from router_inference.router.test_glm4air_router import TestGLM4AirRouter
 
-__all__ = ["BaseRouter", "ExampleRouter", "TestGLM4AirRouter"]
+__all__ = ["BaseRouter", "ExampleRouter"]
